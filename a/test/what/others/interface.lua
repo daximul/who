acfc530f313b53b265142d89f79bfcd13b165834
daximul <79027135+daximul@users.noreply.github.com>
2021-02-14 16:14:48 -0500

    Create interface.lua

---
+++ a/test/what/others/interface.lua
@@ -0,0 +1,5 @@
+local Interface = game:GetObjects("rbxassetid://6370630582")[1]
+Interface.Assets.CommandTemplate.Label.TextScaled = true
+Interface.Tooltip.Title.TextScaled = true
+Interface.Tooltip.Border.Frame.Description.TextScaled = true
+return Interface
