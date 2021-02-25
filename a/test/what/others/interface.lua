60750616c9c6a67adc42acf3c256ed3a95e27618
daximul <79027135+daximul@users.noreply.github.com>
2021-02-25 15:28:07 -0500

    Update interface.lua

--- a/test/what/others/interface.lua
+++ a/test/what/others/interface.lua
@@ -1,7 +1,5 @@
-local Interface = game:GetObjects("rbxassetid://6370630582")[1]
+local Interface = game:GetObjects("rbxassetid://6444310315")[1]
 Interface.Assets.CommandTemplate.Label.TextScaled = true
--- Interface.Notification.Border.Frame.Description.Size = UDim2.new(1, 0, 0.689, -5)
--- Interface.Notification.Border.Frame.Description.TextScaled = true
 Interface.Tooltip.Title.TextScaled = true
 Interface.Tooltip.Border.Frame.Description.TextScaled = true
 return Interface
