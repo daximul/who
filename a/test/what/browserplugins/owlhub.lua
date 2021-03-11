831893982e9ed51f33a45c717e34d88063522472
daximul <79027135+daximul@users.noreply.github.com>
2021-03-10 22:47:07 -0500

    Create owlhub.lua

---
+++ a/test/what/browserplugins/owlhub.lua
@@ -0,0 +1,15 @@
+local Plugin = {
+	["PluginName"] = "Owl Hub",
+	["PluginDescription"] = "",
+	["Commands"] = {
+		["owlhub"] = {
+			["ListName"] = "owlhub",
+			["Description"] = "Load Owl Hub",
+			["Aliases"] = {},
+			["Function"] = function(args,speaker)
+				 loadstring(game:HttpGet('https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua'))();
+			end,
+		},
+	},
+}
+return Plugin
