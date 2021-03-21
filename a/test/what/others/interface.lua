8575d8bac6f3309c0190d5fb3ccd606f775118fe
daximul <79027135+daximul@users.noreply.github.com>
2021-03-20 22:49:19 -0400

    Update interface.lua

--- a/test/what/others/interface.lua
+++ a/test/what/others/interface.lua
@@ -1,4 +1,5 @@
 local Interface = game:GetObjects("rbxassetid://6546192933")[1]
+Interface.Assets.CmdFrame:Clone().Parent = Interface.DaUi.CmdFrames
 Interface.Assets.CommandTemplate.Label.TextScaled = true
 Interface.Tooltip.Title.TextScaled = true
 Interface.Tooltip.Border.Frame.Description.TextScaled = true
