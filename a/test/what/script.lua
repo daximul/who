7205e1d0b63927c7692f78c22c6bbd93cbe35eda
daximul <79027135+daximul@users.noreply.github.com>
2021-03-25 13:30:29 -0400

    Update script.lua

--- a/test/what/script.lua
+++ a/test/what/script.lua
@@ -2313,6 +2313,7 @@ spawn(function()
 	BrowserBtn("Lag Server", "Lag Server", "Lag the Server. You will lag for 6 seconds before it works.", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/lagserver.lua'))();")
 	BrowserBtn("Chat Translator", "Chat Translator", "Translate Chat and Reply\n\nhttps://en.wikipedia.org/wiki/List_of_ISO_639-1_codes\n\nYou have to look the 639-1 column to get a language", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/chattranslator.lua'))();")
 	BrowserBtn("Universal Bhop", "Universal Bhop", "Notice: this is a per-character command, meaning if you die and your body parts disintegrate, you need to execute this again, however with generic deaths it should work fine", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/universalbhop.lua'))();")
+	BrowserBtn("Toon ESP", "Toon ESP", "Load my ESP Script", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/toonesp.lua'))();")
 end)
 --// End of Setup
 
