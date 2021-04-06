246fa43875e96fbf612ac8afd62b286885e9a703
daximul <79027135+daximul@users.noreply.github.com>
2021-04-05 23:24:23 -0400

    Update tinv.lua

--- a/test/what/others/tinv.lua
+++ a/test/what/others/tinv.lua
@@ -1,22 +1,16 @@
-Local = game:GetService('Players').LocalPlayer
-Char  = Local.Character
-touched,tpdback = false, false
-Local.CharacterAdded:connect(function(char)
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Char  = LocalPlayer.Character
+local touched = false
+local tpdback = false
+LocalPlayer.CharacterAdded:connect(function(char)
     if script.Disabled ~= true then
         wait(.25)
         loc = Char.HumanoidRootPart.Position
-        Char:MoveTo(box.Position + Vector3.new(0,.5,0))
+        Char:MoveTo(box.Position + Vector3.new(0, .5, 0))
     end
 end)
-game:GetService('UserInputService').InputBegan:connect(function(key)
-    if key.KeyCode == Enum.KeyCode.Equals then
-        if script.Disabled ~= true then
-            script.Disabled = true
-            print'you may re-execute'
-        end
-    end
-end)
-box = Instance.new('Part',workspace)
+box = Instance.new("Part", workspace)
 box.Anchored = true
 box.CanCollide = true
 box.Size = Vector3.new(10,1,10)
@@ -42,4 +36,4 @@ box.Touched:connect(function(part)
 end)
 repeat wait() until Char
 loc = Char.HumanoidRootPart.Position
-Char:MoveTo(box.Position + Vector3.new(0,.5,0))
+Char:MoveTo(box.Position + Vector3.new(0, .5, 0))
