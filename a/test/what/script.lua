674149997377742b2dd273f48d98ea6188b01426
daximul <79027135+daximul@users.noreply.github.com>
2021-04-05 22:27:29 -0400

    Update script.lua

--- a/test/what/script.lua
+++ a/test/what/script.lua
@@ -2368,6 +2368,7 @@ spawn(function()
 	BrowserBtn("Chat Translator", "Chat Translator", "Translate Chat and Reply\n\nhttps://en.wikipedia.org/wiki/List_of_ISO_639-1_codes\n\nYou have to look the 639-1 column to get a language", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/chattranslator.lua'))();")
 	BrowserBtn("Universal Bhop", "Universal Bhop", "Notice: this is a per-character command, meaning if you die and your body parts disintegrate, you need to execute this again, however with generic deaths it should work fine", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/universalbhop.lua'))();")
 	BrowserBtn("Toon ESP", "Toon ESP", "Load my ESP Script", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/toonesp.lua'))();")
+	BrowserBtn("Drag & Resize Chat", "Drag & Resize Chat", "Make the Default ROBLOX Chat Draggable and Resizable", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/daximul/who/main/a/test/what/browserplugins/dragresizechat.lua'))();")
 end)
 --// End of Setup
 
