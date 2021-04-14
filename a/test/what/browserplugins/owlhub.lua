3ce89f575fd6f6068fa0ebd0196144c9f69861e1
daximul <79027135+daximul@users.noreply.github.com>
2021-04-14 13:31:14 -0400

    Update owlhub.lua

--- a/test/what/browserplugins/owlhub.lua
+++ a/test/what/browserplugins/owlhub.lua
@@ -7,7 +7,8 @@ local Plugin = {
 			["Description"] = "Load Owl Hub",
 			["Aliases"] = {},
 			["Function"] = function(args,speaker)
-				 loadstring(game:HttpGet("https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua"))();
+				 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua"))();
+				loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/newLoad/master/owlhub.lua"))();
 			end,
 		},
 	},
