a90dc4d4143aa4ead31959877098942212b6507d
daximul <79027135+daximul@users.noreply.github.com>
2021-05-06 15:12:57 -0400

    Update black_cmdbar.lua

--- a/test/what/themes/black_cmdbar.lua
+++ a/test/what/themes/black_cmdbar.lua
@@ -1,8 +1,8 @@
 --// Black Command Bar
 --// Transparency is 0.7
 loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/daximul/who/main/a/test/what/script.lua")))();
-pcall(function() while (not DA_PUBLIC_USER_BUILD) do wait() end end)
-local Build = DA_PUBLIC_USER_BUILD
+while (not getgenv()["da_env"]) and (not getgenv()["da_env"]["loaded"]) do wait() end
+local Build = getgenv()["da_env"]
 Build.Interface.Main.BackgroundTransparency = 0.7
 Build.Interface.Main.BackgroundColor3 = Color3.fromRGB(24, 21, 19)
 Build.Interface.Main.Box.BackgroundColor3 = Color3.fromRGB(29, 25, 23)
