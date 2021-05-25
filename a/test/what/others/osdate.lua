f90d4c8834842844c92ab1e56d4c4e5871a38dfd
daximul <79027135+daximul@users.noreply.github.com>
2021-05-24 21:50:54 -0400

    Update osdate.lua

--- a/test/what/others/osdate.lua
+++ a/test/what/others/osdate.lua
@@ -1,37 +1,40 @@
-local Text = ""
+local Text = ("")
 local Date = os.date("*t")
+local Month = Date["month"]
 
 local Months = {
-	January = 1,
-	February = 2,
-	March = 3,
-	April = 4,
-	May = 5,
-	June = 6,
-	July = 7,
-	August = 8,
-	September = 9,
-	October = 10,
-	November = 11,
-	December = 12
+	["January"] = 1,
+	["February"] = 2,
+	["March"] = 3,
+	["April"] = 4,
+	["May"] = 5,
+	["June"] = 6,
+	["July"] = 7,
+	["August"] = 8,
+	["September"] = 9,
+	["October"] = 10,
+	["November"] = 11,
+	["December"] = 12
 }
 
-if Date["month"] == Months.April then
+if Month == Months["January"] then
+	Text = ("🎉 Dark Admin 🎉")
+elseif Month == Months["April"] then
 	Text = ("🥚 Dark Admin 🥚")
-elseif Date["month"] == Months.October then
+elseif Month == Months["October"] then
 	Text = ("🎃 Dark Admin 🎃")
-elseif Date["month"] == Months.December then
+elseif Month == Months["December"] then
 	Text = ("🎄 Dark Admin 🎄")
-elseif Date["month"] == Months.November then
+elseif Month == Months["November"] then
 	Text = ("🦃 Dark Admin 🦃")
-elseif Date["month"] == Months.March then
+elseif Month == Months["March"] then
 	Text = ("🍀 Dark Admin 🍀")
-elseif Date["month"] == Months.February then
+elseif Month == Months["February"] then
 	Text = ("❤️ Dark Admin ❤️")
 else
 	Text = ("Dark Admin")
 end
 
-if Text == "" then Text = "Dark Admin" end
+if (Text == "") then Text = ("Dark Admin") end
 
 return Text
