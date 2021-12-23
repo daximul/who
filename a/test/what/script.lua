e0943e0afd6331144c024e69e60d4224ee12826d
daximul <79027135+daximul@users.noreply.github.com>
2021-12-22 21:59:04 -0500

    Update script.lua

--- a/test/what/script.lua
+++ a/test/what/script.lua
@@ -64,7 +64,20 @@ local Import = function(Asset)
 	end
 end
 
-Import("asset_creator.lua")
+if isfolder and makefolder and isfile and writefile then
+	if not isfolder("Dark Admin") then
+		makefolder("Dark Admin")
+	end
+	if not isfolder("Dark Admin/Plugins") then
+		makefolder("Dark Admin/Plugins")
+	end
+	if not isfolder("Dark Admin/Logs") then
+		makefolder("Dark Admin/Logs")
+	end
+	if not isfolder("Dark Admin/Scripts") then
+		makefolder("Dark Admin/Scripts")
+	end
+end
 
 Prote = Import("prote.lua")
 
