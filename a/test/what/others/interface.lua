af2850e1177b1b99052b8924ea6b808c66c207d6
daximul <79027135+daximul@users.noreply.github.com>
2021-12-22 21:58:04 -0500

    Update interface.lua

--- a/test/what/others/interface.lua
+++ a/test/what/others/interface.lua
@@ -1,5 +1,9 @@
-local _GetPlayers = game:GetService("Players")
-local _GetMarket = game:GetService("MarketplaceService")
+local game = game
+local Connection = game.Loaded
+local CConnect = Connection.Connect
+local GetService = game.GetService
+local _GetPlayers = GetService(game, "Players")
+local _GetMarket = GetService(game, "MarketplaceService")
 local Interface = game:GetObjects("rbxassetid://7841745482")[1]
 Interface.CommandBar.Position = UDim2.new(0.5, -100, 1, 5)
 Interface.CMDS.Position = UDim2.new(0.694, -75, 10, -105)
@@ -10,10 +14,10 @@ spawn(function()
 	Interface.MainDragFrame.Main.Pages.Server.Game.Id.Text = tostring(game.PlaceId)
 	Interface.MainDragFrame.Main.Pages.Server.Game.By.Text = ("By " .. '<font color="rgb(140, 144, 150)"><b>' .. tostring(_GetPlayers:GetNameFromUserIdAsync(game.CreatorId)) .. "</b></font>")
 	Interface.MainDragFrame.Main.Pages.Server.Game.Title.Text = tostring(_GetMarket:GetProductInfo(game.PlaceId).Name)
-	Interface.MainDragFrame.Main.Pages.Server.Game.Description.DescriptionFrame.Description.Text = tostring(_GetMarket:GetProductInfo(game.PlaceId).Description)
+	Interface.MainDragFrame.Main.Pages.Server.Game.Description.DescriptionFrame.Description.Text = tostring(_GetMarket.GetProductInfo(_GetMarket, game.PlaceId).Description)
 	Interface.MainDragFrame.Main.Pages.Server.Game.Thumbnail.Image = ("https://www.roblox.com/asset-thumbnail/image?assetId=" .. tostring(game.PlaceId) .. "&width=768&height=432&format=png")
 	local _GetRandomMessage = loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/daximul/u9yh45/main/m/p.lua")))();
-	if _GetRandomMessage:len() >= 70 then Interface.MainDragFrame.Main.Pages.Menu.Message.Title.TextScaled = true end
+	if string.len(_GetRandomMessage) >= 70 then Interface.MainDragFrame.Main.Pages.Menu.Message.Title.TextScaled = true end
 	Interface.MainDragFrame.Main.Pages.Menu.Message.Title.Text = _GetRandomMessage
 	while wait(1) do
 		Interface.MainDragFrame.Main.Pages.Server.Players.PlayersFrame.Players.Text = ('<font color="rgb(140, 144, 150)">' .. tostring(#_GetPlayers:GetPlayers()) .. '</font>/<font color="rgb(140, 144, 150)">' .. tostring(_GetPlayers.MaxPlayers) .. '</font>')
@@ -21,29 +25,30 @@ spawn(function()
 	end
 end)
 spawn(function()
-	local tweenService = game:GetService("TweenService")
+	local tweenService = GetService(game, "TweenService")
+	local IsA, GetChildren = game.IsA, game.GetChildren
 	local InterfaceTweeningDebounce = false
 	local tweenColor = function(instance, rgb, t1me)
 		local tweenGoals = {TextColor3 = rgb}
 		local tweenInfo = TweenInfo.new(t1me, Enum.EasingStyle.Linear, Enum.EasingDirection.In)
-		local tween = tweenService:Create(instance, tweenInfo, tweenGoals)
-		tween:Play()
+		local tween = tweenService.Create(tweenService, instance, tweenInfo, tweenGoals)
+		tween.Play(tween)
 	end
 	local tweenImageColor = function(instance, rgb, t1me)
 		local tweenGoals = {ImageColor3 = rgb}
 		local tweenInfo = TweenInfo.new(t1me, Enum.EasingStyle.Linear, Enum.EasingDirection.In)
-		local tween = tweenService:Create(instance, tweenInfo, tweenGoals)
-		tween:Play()
+		local tween = tweenService.Create(tweenService, instance, tweenInfo, tweenGoals)
+		tween.Play(tween)
 	end
-	for i,v in pairs(Interface.MainDragFrame.Main.Menu:GetChildren()) do
-		if v:IsA("TextButton") then
-			v.MouseButton1Down:Connect(function()
+	for i,v in pairs(GetChildren(Interface.MainDragFrame.Main.Menu)) do
+		if IsA(v, "TextButton") then
+			CConnect(v.MouseButton1Down, function()
 				if InterfaceTweeningDebounce == true then return end
 				InterfaceTweeningDebounce = true
 				tweenColor(v.PageName, Color3.fromRGB(255, 255, 255), 0.2)
 				tweenImageColor(v.Image, Color3.fromRGB(255, 255, 255), 0.2)
-				for i2,v2 in pairs(Interface.MainDragFrame.Main.Menu:GetChildren()) do
-					if (v2:IsA("TextButton")) and (v2 ~= v) then
+				for i2,v2 in pairs(GetChildren(Interface.MainDragFrame.Main.Menu)) do
+					if (IsA(v2, "TextButton")) and (v2 ~= v) then
 						tweenColor(v2.PageName, Color3.fromRGB(208, 205, 201), 0.2)
 						tweenImageColor(v2.Image, Color3.fromRGB(208, 205, 201), 0.2)
 					end
